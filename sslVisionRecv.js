38e2660cdaa1a5c01cca6fc19d62e52f7df8b856
Shuhei TAKASUGI <bd.c211@gmail.com>
2016-04-17 18:23:39 +0900

    :heavy_plus_sign: add legacy protobuf-mode

--- sslVisionRecv.js
+++ sslVisionRecv.js
@@ -6,8 +6,15 @@ var sslVisionConfig = function() {
   var VISION_PORT = 10006,
       HOST = '0.0.0.0';
 
-  var builder = proto.loadProtoFile ("proto/messages_robocup_ssl_wrapper.proto"),
-      SSL_WrapperPacket = builder.build ("SSL_WrapperPacket");
+  var protoFilePath = 'proto/messages_robocup_ssl_wrapper.proto';
+  var protoName = 'SSL_WrapperPacket';
+  if (process.env.LEGACY) {
+    protoFilePath = 'proto/messages_robocup_ssl_wrapper_legacy.proto';
+    protoName = 'RoboCup2014Legacy.Wrapper.SSL_WrapperPacket';
+  }
+
+  var builder = proto.loadProtoFile (protoFilePath),
+      SSL_WrapperPacket = builder.build (protoName);
 
   var vision_client = dgram.createSocket ('udp4');
   vision_client.on ('listening', function () {
@@ -15,7 +22,7 @@ var sslVisionConfig = function() {
     console.log ('UDP Vision_Client listening on ' + address.address + ":" + address.port);
     vision_client.setBroadcast (true);
     vision_client.setMulticastTTL (128);
-    vision_client.addMembership ('224.5.23.2', HOST);
+    vision_client.addMembership ('224.5.23.2');
   });
 
   vision_client.on('message', function (message, remote) {
